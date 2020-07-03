01c7583d47f2e2d4415ed2dbf96ad8f6fb029327
SyedIftekharuddin <syedy636@gmail.com>
2020-07-03 11:37:47 +0200

    Adding Pojos . closes #1

---
+++ pet-clinic-app-springboot/src/main/java/com/app/utb/petclinicappspringboot/model/Owner.java
@@ -0,0 +1,4 @@
+package com.app.utb.petclinicappspringboot.model;
+
+public class Owner extends Person{
+}
---
+++ pet-clinic-app-springboot/src/main/java/com/app/utb/petclinicappspringboot/model/Vet.java
@@ -0,0 +1,4 @@
+package com.app.utb.petclinicappspringboot.model;
+
+public class Vet extends Person {
+}
