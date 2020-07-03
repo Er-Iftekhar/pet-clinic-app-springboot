61b56387940e87420f4031e8e46f0633221950f2
SyedIftekharuddin <syedy636@gmail.com>
2020-07-03 09:18:41 +0200

    Project Add

---
+++ project-pet-clinic-first/src/main/java/com/app/utb/projectpetclinicfirst/ProjectPetClinicFirstApplication.java
@@ -0,0 +1,13 @@
+package com.app.utb.projectpetclinicfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPetClinicFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPetClinicFirstApplication.class, args);
+    }
+
+}
---
+++ project-pet-clinic-first/src/test/java/com/app/utb/projectpetclinicfirst/ProjectPetClinicFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.utb.projectpetclinicfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPetClinicFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
