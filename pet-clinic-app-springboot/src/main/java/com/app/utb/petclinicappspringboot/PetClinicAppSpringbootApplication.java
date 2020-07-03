8f38e63e94412b2fd81ab7bf0407ff64bdd0329e
SyedIftekharuddin <syedy636@gmail.com>
2020-07-03 11:21:31 +0200

    Intial Project

---
+++ pet-clinic-app-springboot/src/main/java/com/app/utb/petclinicappspringboot/PetClinicAppSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.app.utb.petclinicappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicAppSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicAppSpringbootApplication.class, args);
+    }
+
+}
---
+++ pet-clinic-app-springboot/src/test/java/com/app/utb/petclinicappspringboot/PetClinicAppSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.utb.petclinicappspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicAppSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
